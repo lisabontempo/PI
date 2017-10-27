7cc10100cc48e617e04893685c792fea4265abb1
lisabomtempo <lisabomtempo>
2017-10-27 19:02:22 -0200

    finalização do campo da idade

--- app/src/main/java/psicobaby/psicobaby/MainActivity.java
+++ app/src/main/java/psicobaby/psicobaby/MainActivity.java
@@ -6,46 +6,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText edtIdade;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button ok =    (Button) findViewById(R.id.exibirAlerta);
-        ok.setOnClickListener(new View.OnClickListener(){
+        edtIdade = (EditText) findViewById(R.id.edtIdade);
+
+
+        Button ok = (Button) findViewById(R.id.exibirAlerta);
+        ok.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
-                alerta.setTitle("Vamos começar!");
+                alerta.setTitle("Vamos começar!" + edtIdade.getText());
                 alerta.setIcon(R.mipmap.ic_launcher);
-                alerta.setMessage("Digite a idade do seu bebê.");
+                alerta.setMessage("Agora você terá acesso a dicas desenvolvidas para a idade do seu bebê");
                 alerta.setCancelable(false);
-                alerta.setNegativeButton("cancelar", new DialogInterface.OnClickListener(){
+
+
+                alerta.setPositiveButton("ok", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "Cancelar escolhido", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Dicas para crianças de " + edtIdade.getText() + "anos", Toast.LENGTH_LONG).show();
+
                     }
                 });
 
-                alerta.setPositiveButton("ok", new DialogInterface.OnClickListener(){
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "ok escolhido", Toast.LENGTH_SHORT).show();
-                    }
-                });
 
                 AlertDialog alertDialog = alerta.create();
                 alertDialog.show();
             }
 
 
-
         });
+
+//        public int teste(){
+//        if((edtIdade.getText().toString()=="0")||(edtIdade.getText().toString()=="")||(edtIdade.getText().toString()=="2"){
+//            return 0;
+//        }
+
+//        else{
+//            return 1;
+//        }
+
     }
-}
+
+
+    }
+
+
+
+
+
+
