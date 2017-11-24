053fb7a34b7e07df8c374358d13000207a104de2
Camila <camilanobre170@gmail.com>
2017-11-24 17:40:58 -0200

    Inserção de material no XML do umAno

--- app/src/main/java/psicobaby/psicobaby/ReceberNovidadesActivity.java
+++ app/src/main/java/psicobaby/psicobaby/ReceberNovidadesActivity.java
@@ -1,8 +1,14 @@
 package psicobaby.psicobaby;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.junit.Ignore;
 
@@ -34,16 +40,36 @@ public class ReceberNovidadesActivity extends AppCompatActivity {
         recebeIdade = (EditText) findViewById(R.id.recebeIdade);
         recebeAssunto = (EditText) findViewById(R.id.recebeAssunto);
         recebeMes = (EditText) findViewById(R.id.recebeAssunto);
-        NomeUtil nomeUtil = new  NomeUtil();
-        nome = nomeUtil.ConvertNome(recebeNome.getText().toString());
-        telefone = nomeUtil.ConvertTelefone(recebeTelefone.getText().toString());
-        email = nomeUtil.ConvertEmail(recebeEmail.getText().toString());
-        idade = nomeUtil.ConvertIdade(recebeIdade.getText().toString());
-        assunto = nomeUtil.ConvertAssunto(recebeAssunto.getText().toString());
-        mes = nomeUtil.ConvertMes(recebeMes.getText().toString());
+
+       final Button button = (Button) findViewById(R.id.buttonReceberNovidades);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alertaCadastro();
+            }
+        });
+
+       }
 
 
+    private void alertaCadastro() {
+        AlertDialog.Builder alerta = new AlertDialog.Builder(ReceberNovidadesActivity.this);
+        alerta.setTitle("Cadastro realizado com sucesso!");
+        alerta.setIcon(R.mipmap.ic_launcher);
+        NomeUtil nomeUtil = new NomeUtil();
+        alerta.setMessage(nomeUtil.ConvertNome(recebeNome.getText().toString())+"\n"+nomeUtil.ConvertTelefone(recebeTelefone.getText().toString())
+        +"\n"+nomeUtil.ConvertEmail(recebeEmail.getText().toString())+"\n"+nomeUtil.ConvertIdade(recebeIdade.getText().toString()+"\n")+
+        nomeUtil.ConvertMes(recebeMes.getText().toString())+"\n"+nomeUtil.ConvertAssunto(recebeAssunto.getText().toString()));
+        alerta.setCancelable(false);
+        alerta.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent0 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent0);
+
+            }
+        });
+        AlertDialog alertDialog = alerta.create();
+        alertDialog.show();
     }
-
-
 }
+
